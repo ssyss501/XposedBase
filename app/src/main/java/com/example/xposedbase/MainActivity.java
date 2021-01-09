0c3280ab57e5010f0e000c87099db75504711719
ssyss501 <445228@qq.com>
2021-01-09 14:09:01 +0800

    三个测试按钮

--- app/src/main/java/com/example/xposedbase/MainActivity.java
+++ app/src/main/java/com/example/xposedbase/MainActivity.java
@@ -16,65 +16,97 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.button);
-        final EditText phone_edt = (EditText) findViewById(R.id.phone);
-        final EditText message_edt = (EditText) findViewById(R.id.message);
 
-        final EditText phone2_edt = (EditText) findViewById(R.id.IDC_PHONE2_ED);
-        final EditText path_edt = (EditText) findViewById(R.id.IDC_PATH_ED);
-        final Button sendpic_bt = (Button) findViewById(R.id.IDC_SENDPIC_BT);
-        final Button start_bt = (Button) findViewById(R.id.IDC_START_BT);
-
-        final Button line_test_bt = (Button)findViewById(R.id.IDC_LINETEST_BT);
-        sendpic_bt.setOnClickListener(new View.OnClickListener() {
+        Button viber_bt = findViewById(R.id.IDC_VIBERTEST_BT);
+        viber_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                String phone2 = phone2_edt.getText().toString();
-                String path = path_edt.getText().toString();
-                Intent intent = new Intent("com.Xposebase.onClick2");
-                intent.putExtra("phone", phone2);
-                intent.putExtra("path", path);
+                Intent intent = new Intent("com.xbase.viber.test");
                 sendBroadcast(intent);
             }
         });
-        btn.setOnClickListener(new View.OnClickListener() {
+
+
+        Button wa_bt = findViewById(R.id.IDC_WA_TEST);
+        wa_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String messagae = message_edt.getText().toString();
-                String phone = phone_edt.getText().toString();
-                Intent intent = new Intent("com.Xposebase.onClick");
-                intent.putExtra("messagae", messagae);
-                intent.putExtra("phone", phone);
-                sendBroadcast(intent);
-
-            }
-        });
-
-        start_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent();
-                ComponentName cmp = new ComponentName("com.whatsapp", "com.whatsapp.HomeActivity");
-                intent2.setAction(Intent.ACTION_MAIN);
-                intent2.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent2.setComponent(cmp);
-                startActivity(intent2);
-            }
-        });
-
-        line_test_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String messagae = message_edt.getText().toString();
-                String phone = phone_edt.getText().toString();
-                Intent intent = new Intent("com.Xposebase.line.onClick");
-                intent.putExtra("messagae", messagae);
-                intent.putExtra("phone", phone);
+                Intent intent = new Intent("com.xbase.wa.test");
                 sendBroadcast(intent);
             }
         });
+
+
+        Button line_bt = findViewById(R.id.IDC_LINETEST_BT);
+        line_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.xbase.wa.line");
+                sendBroadcast(intent);
+            }
+        });
+
+
+
+//        btn = (Button) findViewById(R.id.button);
+//        final EditText phone_edt = (EditText) findViewById(R.id.phone);
+//        final EditText message_edt = (EditText) findViewById(R.id.message);
+//
+//        final EditText phone2_edt = (EditText) findViewById(R.id.IDC_PHONE2_ED);
+//        final EditText path_edt = (EditText) findViewById(R.id.IDC_PATH_ED);
+//        final Button sendpic_bt = (Button) findViewById(R.id.IDC_SENDPIC_BT);
+//        final Button start_bt = (Button) findViewById(R.id.IDC_START_BT);
+//
+//        final Button line_test_bt = (Button)findViewById(R.id.IDC_LINETEST_BT);
+//        sendpic_bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//                String phone2 = phone2_edt.getText().toString();
+//                String path = path_edt.getText().toString();
+//                Intent intent = new Intent("com.Xposebase.onClick2");
+//                intent.putExtra("phone", phone2);
+//                intent.putExtra("path", path);
+//                sendBroadcast(intent);
+//            }
+//        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String messagae = message_edt.getText().toString();
+//                String phone = phone_edt.getText().toString();
+//                Intent intent = new Intent("com.Xposebase.onClick");
+//                intent.putExtra("messagae", messagae);
+//                intent.putExtra("phone", phone);
+//                sendBroadcast(intent);
+//
+//            }
+//        });
+//
+//        start_bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent2 = new Intent();
+//                ComponentName cmp = new ComponentName("com.whatsapp", "com.whatsapp.HomeActivity");
+//                intent2.setAction(Intent.ACTION_MAIN);
+//                intent2.addCategory(Intent.CATEGORY_LAUNCHER);
+//                intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                intent2.setComponent(cmp);
+//                startActivity(intent2);
+//            }
+//        });
+//
+//        line_test_bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String messagae = message_edt.getText().toString();
+//                String phone = phone_edt.getText().toString();
+//                Intent intent = new Intent("com.Xposebase.line.onClick");
+//                intent.putExtra("messagae", messagae);
+//                intent.putExtra("phone", phone);
+//                sendBroadcast(intent);
+//            }
+//        });
     }
 }
